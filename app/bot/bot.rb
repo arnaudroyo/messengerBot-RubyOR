ce04f5fd359a8a1e5abf9bed1900c96f7231ea9e
Ubuntu <ubuntu@ip-172-31-23-23.eu-west-3.compute.internal>
2020-05-14 21:32:42 +0000

    bot reply improved

--- app/bot/bot.rb
+++ app/bot/bot.rb
@@ -8,18 +8,20 @@ Facebook::Messenger::Subscriptions.subscribe(
 )
 
 Bot.on :message do |message|
-    if message.text.include?("Bonjour") || message.text.include?("Salut") || message.text.include?("Coucou")
+    if message.text.include?("onjour") || message.text.include?("alut") || message.text.include?("oucou") || message.text.include?("Hey")
         message.reply(text: 'Hey !')
-    elsif message.text.include?("a+") || message.text.include?("Aurevoir") || message.text.include?("A+")
+    elsif message.text.include?("a+") || message.text.include?("urevoir") || message.text.include?("A+")
         message.reply(text: 'Aller salut !')
     elsif message.text.include?("a va")
         message.reply(text: 'Imhotep')
     elsif message.text.include?("ui est la plus belle") || message.text.include?("ui est la meilleure") 
         message.reply(text: 'Clara Darchand <3')
     elsif message.text.include?("^^") 
-        message.reply(text: '^^ rigolo cet emoji ^^')
+        message.reply(text: '^^ rigolo cet emoji')
     elsif message.text.include?("oh") || message.text.include?("Oh")  
         message.reply(text: 'Empressionant n\'est ce pas ;)')
+    elsif message.text.include?("qui") || message.text.include?("Qui")|| || message.text.include?("où")|| message.text.include?("Où") || message.text.include?("omment") || message.text.include?("uand")
+        message.reply(text: 'Je ne sais pas ^^')
     else
         message.reply(text: 'Mmm..?')
     end
