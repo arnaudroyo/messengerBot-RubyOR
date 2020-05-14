68b6fe26406d8eba3b50e679ebd62c1fff96c9fc
Ubuntu <ubuntu@ip-172-31-23-23.eu-west-3.compute.internal>
2020-05-14 11:02:20 +0000

    bot.rb really really fixed

--- app/bot/bot.rb
+++ app/bot/bot.rb
@@ -3,7 +3,7 @@ include Facebook::Messenger
 
 Facebook::Messenger::Subscriptions.subscribe(
   access_token: ENV["ACCESS_TOKEN"],
-  subscribed_fields: %w[feed mention name]
+  subscribed_fields: []
 )
 
 Bot.on :message do |message|
